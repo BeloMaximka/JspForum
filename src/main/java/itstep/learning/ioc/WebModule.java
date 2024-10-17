a91427f47478d05f815a40838386dc8c04702ec2
BeloMaximka <belomaxim@gmail.com>
2024-10-17 12:22:48 +0300

    Implemented basic authentication

--- src/main/java/itstep/learning/ioc/WebModule.java
+++ src/main/java/itstep/learning/ioc/WebModule.java
@@ -1,6 +1,7 @@
 package itstep.learning.ioc;
 
 import com.google.inject.servlet.ServletModule;
+import itstep.learning.filters.BasicAuthenticationFilter;
 import itstep.learning.filters.GlobalErrorHandlingFilter;
 import itstep.learning.servlets.HomeServlet;
 import itstep.learning.servlets.NotFoundServlet;
@@ -10,7 +11,9 @@ public class WebModule  extends ServletModule {
     protected void configureServlets() {
         filter("*").through(GlobalErrorHandlingFilter.class);
 
+        filter("/home").through(BasicAuthenticationFilter.class);
         serve("/home").with(HomeServlet.class);
+
         serve("*").with(NotFoundServlet.class);
     }
 }
--- src/main/java/itstep/learning/servlets/HomeServlet.java
+++ src/main/java/itstep/learning/servlets/HomeServlet.java
@@ -1,16 +1,12 @@
 package itstep.learning.servlets;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.inject.Singleton;
 import itstep.learning.expections.HttpException;
-import itstep.learning.models.WelcomeMessage;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Singleton
 public class HomeServlet extends HttpServlet {
